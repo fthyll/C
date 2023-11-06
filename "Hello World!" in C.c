96b237fa520fd731880ca85909c8f452101c7d99
Muhammad Fatih Yumna Lajuwirdi Lirrahman <m.fatihyumna25@gmail.com>
2023-11-07 04:19:41 +0700

    Refactor input method and print output

--- "Hello World!" in C.c
+++ "Hello World!" in C.c
@@ -5,11 +5,12 @@
 
 int main() 
 {
-	
+    
     char s[100];
-    scanf("%[^\n]%*c", &s);
-  	
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
-    printf("Hello, World!\n%s", s);
+    gets(s); 
+    
+      printf("Hello, World!\n");
+      printf("%s",s);
+        
     return 0;
 }
