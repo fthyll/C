806b9efe2cff3c92f06f60f0ef5da27969e20920
Muhammad Fatih Yumna Lajuwirdi Lirrahman <m.fatihyumna25@gmail.com>
2023-11-11 03:23:41 +0700

    Refactored string sorting functions

--- Sorting Array of Strings.c
+++ Sorting Array of Strings.c
@@ -2,51 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 int lexicographic_sort(const char* a, const char* b) {
-    for (int i = 0; i < strlen(a); i++) {
-        if (a[i] < b[i]) return 1;
-        else if (a[i] > b[i]) return 0;
-    }
+    for (; *a && *b && *a == *b; ++a, ++b)
+        ;
+    return *a - *b;
 }
 
 int lexicographic_sort_reverse(const char* a, const char* b) {
-    for (int i = 0; i < strlen(a); i++) {
-        if (a[i] > b[i]) return 1;
-        else if (a[i] < b[i]) return 0;
-    }
-
-}
-
-int sort_by_number_of_distinct_characters(const char* a, const char* b) {
-    int arr[26] = {0};
-    for (int i = 0; i < strlen(a); i++) {
-        arr[a[i] - 'a']++;
-    }
-    int count_a = 0;
-    for (int i = 0; i < 26; i++) {
-        if (arr[i] > 0) count_a++;
-    }
-    memset(arr, 0, sizeof(arr));
-    for (int i = 0; i < strlen(b); i++) {
-        arr[b[i] - 'a']++;
-    }
-    int count_b = 0;
-    for (int i = 0; i < 26; i++) {
-        if (arr[i] > 0) count_b++;
-    }
-    if (count_a < count_b) return 1;
-    else if (count_a > count_b) return 0;
-
+    for (; *a && *b && *a == *b; ++a, ++b)
+        ;
+    return *b - *a;
 }
 
 int sort_by_length(const char* a, const char* b) {
-    if (strlen(a) < strlen(b)) return 1;
-    else if (strlen(a) > strlen(b)) return 0;
+    int n = strlen(a), m = strlen(b);
+    if (n == m) return lexicographic_sort(a, b);
+    return n - m;
 }
 
-void string_sort(char** arr,const int len,int (*cmp_func)(const char* a, const char* b)){
+int distinct_characters(const char* s) {
+    char* d_str = (char *) malloc(1);
+    d_str[0] = '\0';
+    for (int ctr = 0, i = 0; s[i]; ++i) {
+        if (strchr(d_str, s[i]) == NULL) {
+            d_str = realloc(d_str, strlen(d_str) + 2);
+            d_str[ctr] = s[i];
+            d_str[++ctr] = '\0';
+        }
+    }
+    int d_chars = strlen(d_str);
+    free(d_str);
+    return d_chars;
+}
+
+int sort_by_number_of_distinct_characters(const char* a, 
+                                          const char* b) {
+    int x = distinct_characters(a), y = distinct_characters(b);
+    if (x == y) return lexicographic_sort(a, b);
+    return x - y;
+}
+
+void string_sort(char** arr, const int len, 
+                 int (*cmp_func)(const char* a, 
+                                 const char* b)) {
     for (int i = 0; i < len; i++) {
-        for (int j = i+1; j < len; j++) {
-            if (cmp_func(arr[i], arr[j])) {
+        for (int j = i + 1; j < len; j++) {
+            if (cmp_func(arr[i], arr[j]) > 0) {
                 char* temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
